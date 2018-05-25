04aa88a2bd1c2f1fbee6b5a22c2040bd64e65ce5
santoharyono <santoharyono@gmail.com>
2018-05-25 10:13:21 +0700

    revert to jdbcTemplate

--- src/main/java/com/mitraiscdc/databasedemo/dao/PersonDaoImpl.java
+++ src/main/java/com/mitraiscdc/databasedemo/dao/PersonDaoImpl.java
@@ -4,44 +4,43 @@ import com.mitraiscdc.databasedemo.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.support.JdbcDaoSupport;
 import org.springframework.stereotype.Repository;
 
 import java.sql.Timestamp;
 import java.util.List;
 
 @Repository
-public class PersonDaoImpl extends JdbcDaoSupport implements PersonDao{
+public class PersonDaoImpl implements PersonDao{
 
-//    @Autowired
-//    private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @Override
     public List<Person> findAll() {
-        return getJdbcTemplate().query("select * from person", new BeanPropertyRowMapper<>(Person.class));
+        return jdbcTemplate.query("select * from person", new BeanPropertyRowMapper<>(Person.class));
     }
 
     @Override
     public Person findById(int id) {
-        return getJdbcTemplate().queryForObject("select * from person where id=?", new Object[]{id},
+        return jdbcTemplate.queryForObject("select * from person where id=?", new Object[]{id},
                 new BeanPropertyRowMapper<>(Person.class));
     }
 
     @Override
     public int deletebyId(int id) {
-        return getJdbcTemplate().update("delete from person where id=?", new Object[]{id});
+        return jdbcTemplate.update("delete from person where id=?", new Object[]{id});
     }
 
     @Override
     public int insert(Person person) {
-        return getJdbcTemplate().update("insert into person(id, name, location, birthdate) value(?,?,?,?)",
+        return jdbcTemplate.update("insert into person(id, name, location, birthdate) value(?,?,?,?)",
                 new Object[]{person.getId(), person.getName(), person.getLocation(),
                         new Timestamp(person.getBirthDate().getTime())});
     }
 
     @Override
     public int update(Person person) {
-        return getJdbcTemplate().update("update person set name = ?, location = ?, birthdate = ?" +
+        return jdbcTemplate.update("update person set name = ?, location = ?, birthdate = ?" +
                         "where id = ?",
                 new Object[]{person.getName(), person.getLocation(), new Timestamp(person.getBirthDate().getTime()),
                 person.getId()});
